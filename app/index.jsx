74f40916b67326a302a4f0503fe8147d8dd0d655
Huy Nguyen <tronghuy0077@gmail.com>
2024-04-14 18:15:50 +0700

    config tailwind and assets

--- app/index.jsx
+++ app/index.jsx
@@ -1,37 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { Link } from 'expo-router';
 
-const PlaceholderImage = require('../assets/martin-katler-94lAQc7ipNg-unsplash.jpg');
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Text>Aora</Text>
-        <Image source={PlaceholderImage} style={styles.image} />
-        <Link href="/profile">Go to profile</Link>
+    <View className="flex-1 items-center justify-center bg-white bt-10">
 
-      </View>
-      
+      <Text >Aora</Text>
+
+      <Link href="/profile">Go to profile</Link>
+
+
+
       <StatusBar style="auto" />
-
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,  
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
-});
+
